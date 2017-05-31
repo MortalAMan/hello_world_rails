908da386ad5190d98d32a041f28f8c56e87244ac
Addy King <ashepherdking@gmail.com>
2017-05-30 22:35:29 -0600

    My first blog site

---
+++ db/migrate/20170530162707_create_blog_posts.rb
@@ -0,0 +1,17 @@
+class CreateBlogPosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blog_posts do |t|
+      t.string :title, null: false
+      t.string :author, null: false
+      t.text :body, null: false
+
+      t.timestamps
+    end
+  end
+end
+ # CREATE TABLE blog_posts (
+ # id serial PRIMARY KEY
+ # title varchar(50) NOT null
+ # author vharchar(50) NOT NULL
+ # body text NOT NULL
+ #)
